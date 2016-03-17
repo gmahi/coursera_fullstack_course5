7d10c5c48bdac773c7bfafe2465ea47457283a3e
gmahi <gullamahi@gmail.com>
2016-03-18 00:24:32 +0100

    after week1 assignment 1

---
+++ node-express/server-5.js
@@ -0,0 +1,26 @@
+﻿var express = require('express');
+var morgan = require('morgan');
+var dishRouter = require('./a1modules/dishRouter')
+var promoRouter = require('./a1modules/promoRouter')
+var leaderRouter = require('./a1modules/leaderRouter')
+
+var hostname = 'localhost';
+var port = 3000;
+
+var app = express();
+
+app.use(morgan('dev'));
+
+
+
+
+
+app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leadership', leaderRouter);
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, hostname, function () {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
